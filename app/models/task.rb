f206fd108ce32f2caa2db1d848a48b597176fc85
Eduardo Teixeira <dunirvana@gmail.com>
2022-04-06 17:11:28 -0300

    See flash messages in the bootstrap alert.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  validates: :description, presence: true
-  validates: :done, inclusion: { in: [true, false]}
+  validates :description, presence: true
+  validates :done, inclusion: { in: [true, false]}
 end
